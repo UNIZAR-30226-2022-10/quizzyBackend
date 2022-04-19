b20d717fc1d49ff4b28a8ca1ac72aa0741720b32
Jaime Martin <jaime14albalate@gmail.com>
2022-04-19 12:58:31 +0200

    friends route without test

--- controllers/rest/friends.js
+++ controllers/rest/friends.js
@@ -9,7 +9,97 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const {
+    validateNickname
+} = require("../../utils/validateInput");
+
 const { StatusCodes } = require("http-status-codes");
 const createError = require("http-errors");
 
 
+async function getFriends(nickname){
+
+    if(!nickname || !validateNickname(nickname)){
+        throw createError(StatusCodes.BAD_REQUEST, "Invalid nickname");
+    }
+    
+    //  GET
+
+    const allFriends = await prisma.friends.findMany(
+        {
+            where: {
+                OR: [
+                    {
+                        nickname_1: nickname,
+
+                        select: {
+                            nickname_2: true
+                        }
+                    },
+                    {
+                        nickname_2: nickname,
+
+                        select: {
+                            nickname_1: true
+                        }
+                    }
+                ]
+            },
+            
+        }
+    );
+
+    return allFriends;
+}
+
+async function addFriend(nickname, friendNickname){
+
+    if(!nickname || !validateNickname(nickname)){
+        throw createError(StatusCodes.BAD_REQUEST, "Invalid nickname");
+    }
+
+    if(!nickname || !validateNickname(nickname)){
+        throw createError(StatusCodes.BAD_REQUEST, "Invalid friend nickname");
+    }
+
+    //  ADD
+
+    return await prisma.friends.create({
+        data: {
+            nickname_1: nickname,
+            nickname_2: friendNickname
+        }
+    });
+}
+
+async function deleteFriend(nickname, friendNickname){
+
+    if(!nickname || !validateNickname(nickname)){
+        throw createError(StatusCodes.BAD_REQUEST, "Invalid nickname");
+    }
+
+    if(!nickname || !validateNickname(nickname)){
+        throw createError(StatusCodes.BAD_REQUEST, "Invalid friend nickname");
+    }
+
+    //  DELETE
+    return await prisma.friends.delete({
+        where: {
+            OR: [
+                {
+                    nickname_1: nickname,
+                    nickname_2: friendNickname
+                },
+                
+                {
+                    nickname_1: friendNickname,
+                    nickname_2: nickname
+                }
+            ]
+        }
+    });
+}
+
+module.exports.getFriends = getFriends;
+module.exports.addFriend = addFriend;
+module.exports.deleteFriend = deleteFriend;
--- routes/friends.js
+++ routes/friends.js
@@ -7,7 +7,9 @@
 var express = require("express");
 const { StatusCodes } = require("http-status-codes");
 const {
-
+    getFriends,
+    addFriend,
+    deleteFriend
 } = require("../controllers/rest/friends");
 
 var friendsRouter = express.Router();
@@ -16,6 +18,87 @@ const { PrismaClientKnownRequestError } = require("@prisma/client");
 
 const { authRestToken } = require('../middleware/auth');
 
+friendsRouter.get("/user/"+ req.jwtUser + "/friends", authRestToken, function(req, res, next){
+    
+    getFriends(req.jwtUser)
+        .then((friends) => {
+            res.statusCode = StatusCodes.OK;
+            res.send({
+                friends: friends,
+                ok: true
+            });
+        })
+        .catch((e) => {
+            res.statusCode = e.status;
+            res.send({
+                msg: e.message,
+                ok: false
+            });
+        });
+});
 
+friendsRouter.post("/user/"+ req.jwtUser + "/friends", authRestToken, function(req, res, next){
+    
+    const{friendNickname} = req.body;
+
+    addFriend(req.jwtUser, friendNickname)
+        .then(() => {
+            res.statusCode = StatusCodes.OK;
+            res.send({
+                msg: "Succesfull operation",
+                ok: true
+            });
+        })
+        .catch((e) => {
+            
+            if (e instanceof PrismaClientKnownRequestError){
+                res.statusCode = StatusCodes.CONFLICT;
+
+                res.send({
+                    msg: "User not found",
+                    ok: false
+                });
+            }
+            else{
+                res.statusCode = e.status;
+                res.send({
+                    msg: e.message,
+                    ok: false
+                });
+            }
+        });
+});
+
+friendsRouter.delete("/user/"+ req.jwtUser + "/friends", authRestToken, function(req, res, next){
+    
+    const{friendNickname} = req.body;
+
+    deleteFriend(req.jwtUser, friendNickname)
+        .then(() => {
+            res.statusCode = StatusCodes.OK;
+            res.send({
+                msg: "Succesfull operation",
+                ok: true
+            });
+        })
+        .catch((e) => {
+
+            if (e instanceof PrismaClientKnownRequestError){
+                res.statusCode = StatusCodes.CONFLICT;
+
+                res.send({
+                    msg: "User not found",
+                    ok: false
+                });
+            }
+            else{
+                res.statusCode = e.status;
+                res.send({
+                    msg: e.message,
+                    ok: false
+                });
+            }
+        });
+});
 
 module.exports = friendsRouter;
