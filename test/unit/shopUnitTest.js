32901452335e0768554e819ff98dec6112552173
ZenithGD <darimarcos456@hotmail.com>
2022-04-22 00:17:54 +0200

    full shop tests

--- test/unit/shopUnitTest.js
+++ test/unit/shopUnitTest.js
@@ -11,7 +11,6 @@ const { PrismaClient } = require("@prisma/client");
 const { StatusCodes } = require("http-status-codes");
 const { signToken } = require("../../utils/auth");
 
-/* register user tests */
 function getCosmetics(expected) {
     return request(app)
         .get("/shop/cosmetics")
@@ -22,7 +21,6 @@ function getCosmetics(expected) {
         });
 }
 
-/* register user tests */
 function getWildcards(expected) {
     return request(app)
         .get("/shop/wildcards")
@@ -33,7 +31,6 @@ function getWildcards(expected) {
         });
 }
 
-/* register user tests */
 function buyCosmeticTest(token, id, expected) {
     return request(app)
         .post("/shop/cosmetics/buy")
@@ -41,6 +38,20 @@ function buyCosmeticTest(token, id, expected) {
         authorization: token ? `Bearer ${token}` : null})
         .send({
             id: id
+        })
+        .then(async (response) => {
+            await expected(response);
+        });
+}
+
+function buyWildcardTest(token, id, amount, expected) {
+    return request(app)
+        .post("/shop/wildcards/buy")
+        .set({Accept: 'application/json',
+        authorization: token ? `Bearer ${token}` : null})
+        .send({
+            id: id,
+            amount: amount
         })
         .then(async (response) => {
             await expected(response);
@@ -63,6 +74,7 @@ const shopTestSuite = () => describe('Test shop route', () => {
     })
 
     describe('Test cosmetic purchases', () => {
+
         beforeAll(async () => {
             return request(app)
                 .post("/user/")
@@ -84,7 +96,7 @@ const shopTestSuite = () => describe('Test shop route', () => {
 
         describe('Valid classes', () => {
             // buy cosmetic
-            test('EQ 1,2,3,7,9,13,15', async () => {
+            test('EQ 1,2,3,7,8,9,13,15', async () => {
                 return buyCosmeticTest(signToken("usuario"), 2, (response) => {
                     // check wallet
                     request(app)
@@ -157,7 +169,113 @@ const shopTestSuite = () => describe('Test shop route', () => {
                 })
             });
         })
-    })
+    });
+
+    describe('Test wildcard purchases', () => {
+
+        beforeAll(async () => {
+            return request(app)
+                .post("/user/")
+                .set("Accept", "application/json")
+                .send({
+                    nickname: "usuario",
+                    email: "abc@def.gh",
+                    password: "dadada"
+                });
+        });
+
+        afterAll(async () => {
+            return request(app)
+                .delete("/user/")
+                .set({Accept: 'application/json',
+                    Authorization: `Bearer ${signToken("usuario")}` })
+                .send({});
+        });
+
+        describe('Valid classes', () => {
+            // buy cosmetic
+            test('EQ 1,2,3,7,8,9,13,14', async () => {
+                return buyWildcardTest(signToken("usuario"), 1, 3, (response) => {
+                    console.log(response.body);
+                    // check wallet
+                    request(app)
+                        .get("/user")
+                        .set({Accept: 'application/json',
+                            authorization: `Bearer ${signToken("usuario")}`})
+                        .send()
+                        .then(async (response) => {
+                            expect(response.body.wallet).toBe(150);
+                        });
+                    expect(response.status).toBe(StatusCodes.OK);
+                })
+            });
+        });
+
+        describe('Invalid classes', () => {
+            // null token
+            test('EQ 4', async () => {
+                return buyWildcardTest(null, 1, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+                })
+            });
+
+            // malformed token
+            test('EQ 5', async () => {
+                return buyWildcardTest("abcd", 1, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.FORBIDDEN);
+                })
+            });
+
+            // user doesn't exist
+            test('EQ 6', async () => {
+                return buyWildcardTest(signToken("abcd"), 1, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+                })
+            });
+
+            // null id
+            test('EQ 10', async () => {
+                return buyWildcardTest(signToken("usuario"), null, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+                })
+            });
+
+            // invalid id
+            test('EQ 11', async () => {
+                return buyWildcardTest(signToken("usuario"), -1, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+                })
+            });
+
+            // Wildcard doesn't exist
+            test('EQ 12', async () => {
+                return buyWildcardTest(signToken("usuario"), 10000, 3, (response) => {
+                    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+                })
+            });
+
+            // null amount
+            test('EQ 16', async () => {
+                return buyWildcardTest(signToken("usuario"), 1, null, (response) => {
+                    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+                })
+            });
+
+            // invalid amount
+            test('EQ 16', async () => {
+                return buyWildcardTest(signToken("usuario"), 1, -1, (response) => {
+                    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+                })
+            });
+
+            // Not enough coins in wallet
+            test('EQ 16', async () => {
+                return buyWildcardTest(signToken("usuario"), 1, 1000, (response) => {
+                    expect(response.status).toBe(StatusCodes.CONFLICT);
+                })
+            });
+        })
+    });
 })
 
 module.exports = shopTestSuite;
